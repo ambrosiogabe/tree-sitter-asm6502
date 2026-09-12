29a27950e18c488f756a9ec73ba69de73bb2b1ad
Gabriel Ambrosio <ambrosiogabe@gmail.com>
2026-09-12 18:48:06 -0500

    chore: Update gitignore and formatting

--- grammar.js
+++ grammar.js
@@ -1,303 +1,291 @@
-const WS = choice(' ', '\t');
-const NEWLINE = '\n';
+const WS = choice(" ", "\t");
+const NEWLINE = "\n";
 
 module.exports = grammar({
-  name: 'asm6502',
+	name: "asm6502",
 
+	extras: ($) => [
+		/\s/, // whitespace control
+		$.comment,
+	],
 
+	conflicts: ($) => [
+		// operation conflicts (ex: sta #0, or sta 5)
+		[$._immediate_opcode, $._absolute_opcode, $._indirect_opcode],
+		[$._immediate_opcode, $._absolute_opcode],
+		[$._absolute_opcode, $._indirect_opcode],
+	],
 
-  extras: $ => [
-    /\s/, // whitespace control
-    $.comment,
-  ],
+	rules: {
+		source_file: ($) => repeat($._statement),
 
-  conflicts: $ => [
-    // operation conflicts (ex: sta #0, or sta 5)
-    [$._immediate_opcode, $._absolute_opcode, $._indirect_opcode],
-    [$._immediate_opcode, $._absolute_opcode],
-    [$._absolute_opcode, $._indirect_opcode],
-  ],
+		_statement: ($) =>
+			choice($._operation, $.label, $.assignment, $._directive),
 
-  rules: {tree
-    source_file: $ => repeat($._statement),
+		/* grammar misc */
+		comment: ($) => /;.*/,
 
-    _statement: $ => choice(
-      $._operation,
-      $.label,
-      $.assignment,
-      $._directive,
-    ),
+		_ws_sep: ($) => token.immediate(repeat1(WS)),
+		_ws_end: ($) => token.immediate(seq(repeat(WS), NEWLINE)),
 
+		_directive: ($) =>
+			choice(
+				$.file_directive,
+				$.label_directive,
+				$.section_directive,
+				$.imm_directive,
+			),
 
-    /* grammar misc */
-    comment: $ => /;.*/,
+		/* ex: include */
+		file_directive: ($) =>
+			seq(
+				choice($._inc_name),
+				$._ws_sep,
+				alias(choice(/[._a-zA-Z0-9]+/, /"[._a-zA-Z0-9]+"/), $.file_name),
+				//$._ws_end,
+			),
 
-    _ws_sep: $ => token.immediate( repeat1(WS) ),
-    _ws_end: $ => token.immediate( seq(repeat(WS), NEWLINE) ),
+		/* ex: extern */
+		label_directive: ($) =>
+			seq(
+				choice($._ext_name),
+				$._ws_sep,
+				$.global_label,
+				//$._ws_end,
+			),
 
+		section_directive: ($) =>
+			seq(
+				choice($._sec_name),
+				$._ws_sep,
+				alias(/[._a-zA-Z][._a-zA-Z0-9]+/, $.section_name),
+				//$._ws_end,
+			),
 
+		// directive w/ expr
+		imm_directive: ($) =>
+			seq(
+				choice($._word_name, $._byte_name),
+				$._expr,
+				//$._ws_end,
+			),
 
+		_inc_name: ($) => alias(token(seq(optional("."), "include")), $.directive),
+		_ext_name: ($) => alias(token(seq(optional("."), "extern")), $.directive),
+		_sec_name: ($) => alias(token(seq(optional("."), "section")), $.directive),
+		_word_name: ($) => alias(token(seq(optional("."), "word")), $.directive),
+		_byte_name: ($) => alias(token(seq(optional("."), "byte")), $.directive),
 
-    _directive: $ => choice(
-      $.file_directive,
-      $.label_directive,
-      $.section_directive,
-      $.imm_directive,
-    ),
+		assignment: ($) => seq($.global_label, "=", $._expr),
 
-    /* ex: include */
-    file_directive: $ => seq(
-      choice($._inc_name),
-      $._ws_sep,
-      alias(choice(
-        /[._a-zA-Z0-9]+/,
-        /"[._a-zA-Z0-9]+"/,
-      ), $.file_name),
-      //$._ws_end,
-    ),
+		/* labels */
 
-    /* ex: extern */
-    label_directive: $ => seq(
-      choice($._ext_name),
-      $._ws_sep,
-      $.global_label,
-      //$._ws_end,
-    ),
+		label: ($) => seq(choice($.local_label, $.global_label), ":"),
 
-    section_directive: $ => seq(
-      choice($._sec_name),
-      $._ws_sep,
-      alias(/[._a-zA-Z][._a-zA-Z0-9]+/, $.section_name),
-      //$._ws_end,
-    ),
+		/* expression definitions */
 
-    // directive w/ expr
-    imm_directive: $ => seq(
-      choice($._word_name, $._byte_name),
-      $._expr,
-      //$._ws_end,
-    ),
+		_expr: ($) =>
+			choice(
+				$.num_literal,
+				$._identifier,
+				seq("(", $._expr, ")"),
+				$.binary_expr,
+				$.unary_expr,
+			),
 
-    _inc_name:  $ => alias(token( seq(optional('.'), 'include') ), $.directive),
-    _ext_name:  $ => alias(token( seq(optional('.'), 'extern' ) ), $.directive),
-    _sec_name:  $ => alias(token( seq(optional('.'), 'section') ), $.directive),
-    _word_name: $ => alias(token( seq(optional('.'), 'word'   ) ), $.directive),
-    _byte_name: $ => alias(token( seq(optional('.'), 'byte'   ) ), $.directive),
+		_identifier: ($) => choice($.local_label, $.global_label),
 
+		num_literal: ($) =>
+			token(
+				choice(
+					repeat1(/[0-9]/),
+					seq("%", token.immediate(repeat1(/[01]/))),
+					seq("$", token.immediate(repeat1(/[0-9a-fA-F]/))),
+				),
+			),
 
+		binary_expr: ($) =>
+			prec.left(
+				seq(
+					$._expr,
+					alias(choice("+", "-", "*", "/", "<<", ">>", "&", "|"), $.operator),
+					$._expr,
+				),
+			),
+		unary_expr: ($) =>
+			prec(
+				2,
+				seq(
+					// TODO: should this be an operator? or just part of the literal?
+					alias(/[-\+<>~]/, $.operator),
+					$._expr,
+				),
+			),
 
-    assignment: $ => seq(
-      $.global_label,
-      '=',
-      $._expr,
-    ),
+		/* operation definitions */
 
-    /* labels */
+		_operation: ($) =>
+			seq(
+				choice($.implied, $.relative, $.immediate, $.absolute, $.indirect),
+				//$._ws_end,
+			),
 
-    label: $ => seq(
-      choice($.local_label, $.global_label),
-      ':',
-    ),
+		implied: ($) => seq(alias($._implied_opcode, $.opcode)),
+		relative: ($) =>
+			seq(alias($._relative_opcode, $.opcode), $._ws_sep, $._expr),
 
+		immediate: ($) =>
+			seq(
+				alias($._immediate_opcode, $.opcode),
+				$._ws_sep,
+				token.immediate("#"),
+				$._expr,
+			),
+		absolute: ($) =>
+			prec.dynamic(
+				1,
+				seq(
+					alias($._absolute_opcode, $.opcode),
+					$._ws_sep,
+					$._expr,
+					optional(choice($._reg_x, $._reg_y)),
+				),
+			),
+		// indirect comes before absolute
+		indirect: ($) =>
+			prec.dynamic(
+				2,
+				seq(
+					alias($._indirect_opcode, $.opcode),
+					$._ws_sep,
+					token.immediate("("),
+					$._expr,
+					choice(seq(")"), seq($._reg_x, ")"), seq(")", $._reg_y)),
+				),
+			),
 
-    /* expression definitions */
+		_reg_x: ($) => seq(",", alias("x", $.register)),
+		_reg_y: ($) => seq(",", alias("y", $.register)),
 
-    _expr: $ => (choice(
-      $.num_literal,
-      $._identifier,
-      seq('(', $._expr, ')'),
-      $.binary_expr,
-      $.unary_expr,
-    )),
+		/* opcode definitions */
+		_implied_opcode: ($) =>
+			choice(
+				/[nN][oO][pP]/,
+				/[cC][lL][cC]/,
+				/[cC][lL][dD]/,
+				/[cC][lL][iI]/,
+				/[cC][lL][vV]/,
+				/[dD][eE][xX]/,
+				/[dD][eE][yY]/,
+				/[iI][nN][xX]/,
+				/[iI][nN][yY]/,
+				/[sS][eE][cC]/,
+				/[sS][eE][dD]/,
+				/[sS][eE][iI]/,
+				/[sS][tT][pP]/,
+				/[tT][aA][xX]/,
+				/[tT][aA][yY]/,
+				/[tT][sS][xX]/,
+				/[tT][xX][aA]/,
+				/[tT][xX][sS]/,
+				/[tT][yY][aA]/,
+				/[wW][aA][iI]/,
+				// a register
+				/[aA][sS][lL]/,
+				/[dD][eE][cC]/,
+				/[iI][nN][cC]/,
+				/[lL][sS][rR]/,
+				/[rR][oO][lL]/,
+				/[rR][oO][rR]/,
+				// stack based
+				/[bB][rR][kK]/,
+				/[pP][hH][aA]/,
+				/[pP][hH][pP]/,
+				/[pP][hH][xX]/,
+				/[pP][hH][yY]/,
+				/[pP][lL][aA]/,
+				/[pP][lL][pP]/,
+				/[pP][lL][xX]/,
+				/[pP][lL][yY]/,
+				/[rR][tT][iI]/,
+				/[rR][tT][sS]/,
+			),
 
-    _identifier: $ => choice($.local_label, $.global_label),
+		_relative_opcode: ($) =>
+			choice(
+				/[bB][cC][cC]/,
+				/[bB][cC][sS]/,
+				/[bB][eE][qQ]/,
+				/[bB][mM][iI]/,
+				/[bB][nN][eE]/,
+				/[bB][pP][lL]/,
+				/[bB][rR][aA]/,
+				/[bB][vV][cC]/,
+				/[bB][vV][sS]/,
+			),
 
-    num_literal: $ => token(choice(
-      repeat1(/[0-9]/),
-      seq('%', token.immediate(repeat1(/[01]/))),
-      seq('$', token.immediate(repeat1(/[0-9a-fA-F]/)))
-    )),
+		_immediate_opcode: ($) =>
+			choice(
+				/[aA][dD][cC]/,
+				/[aA][nN][dD]/,
+				/[bB][iI][tT]/,
+				/[cC][mM][pP]/,
+				/[cC][pP][xX]/,
+				/[cC][pP][yY]/,
+				/[eE][oO][rR]/,
+				/[lL][dD][aA]/,
+				/[lL][dD][xX]/,
+				/[lL][dD][yY]/,
+				/[oO][rR][aA]/,
+				/[sS][bB][cC]/,
+			),
 
-    binary_expr: $ => prec.left(seq(
-      $._expr,
-      alias(choice('+', '-', '*', '/', '<<', '>>', '&', "|"), $.operator),
-      $._expr,
-    )),
-    unary_expr: $ => prec(2, seq(
-      // TODO: should this be an operator? or just part of the literal?
-      alias(/[-\+<>~]/, $.operator),
-      $._expr,
-    )),
+		_absolute_opcode: ($) =>
+			choice(
+				/[aA][dD][cC]/,
+				/[aA][nN][dD]/,
+				/[aA][sS][lL]/,
+				/[bB][iI][tT]/,
+				/[cC][mM][pP]/,
+				/[cC][pP][xX]/,
+				/[cC][pP][yY]/,
+				/[dD][eE][cC]/,
+				/[eE][oO][rR]/,
+				/[iI][nN][cC]/,
+				/[jJ][mM][pP]/,
+				/[jJ][sS][rR]/,
+				/[lL][dD][aA]/,
+				/[lL][dD][xX]/,
+				/[lL][dD][yY]/,
+				/[lL][sS][rR]/,
+				/[oO][rR][aA]/,
+				/[rR][oO][lL]/,
+				/[rR][oO][rR]/,
+				/[sS][bB][cC]/,
+				/[sS][tT][aA]/,
+				/[sS][tT][xX]/,
+				/[sS][tT][yY]/,
+				/[sS][tT][zZ]/,
+				/[tT][rR][bB]/,
+				/[tT][sS][bB]/,
+			),
 
+		_indirect_opcode: ($) =>
+			choice(
+				// ind abs
+				/[jJ][mM][pP]/,
+				// ind zp
+				/[aA][dD][cC]/,
+				/[aA][nN][dD]/,
+				/[cC][mM][pP]/,
+				/[eE][oO][rR]/,
+				/[lL][dD][aA]/,
+				/[oO][rR][aA]/,
+				/[sS][bB][cC]/,
+				/[sS][tT][aA]/,
+			),
 
-
-
-    /* operation definitions */
-
-    _operation: $ => seq(
-      choice(
-        $.implied,
-        $.relative,
-        $.immediate,
-        $.absolute,
-        $.indirect
-      ),
-      //$._ws_end,
-    ),
-
-    implied: $ => seq( alias($._implied_opcode, $.opcode) ),
-    relative: $ => seq(
-      alias($._relative_opcode, $.opcode),
-      $._ws_sep,
-      $._expr
-    ),
-
-    immediate: $ => seq(
-      alias($._immediate_opcode, $.opcode),
-      $._ws_sep,
-      token.immediate('#'),
-      $._expr,
-    ),
-    absolute: $ => prec.dynamic(1, seq(
-      alias($._absolute_opcode, $.opcode),
-      $._ws_sep,
-      $._expr,
-      optional(choice($._reg_x, $._reg_y)),
-    )),
-    // indirect comes before absolute
-    indirect: $ => prec.dynamic(2, seq(
-      alias($._indirect_opcode, $.opcode),
-      $._ws_sep,
-      token.immediate('('),
-      $._expr,
-      choice(
-        seq(')'),
-        seq($._reg_x, ')'),
-        seq(')', $._reg_y),
-      ),
-    )),
-
-    _reg_x: $ => seq(',', alias('x', $.register)),
-    _reg_y: $ => seq(',', alias('y', $.register)),
-
-
-
-
-
-    /* opcode definitions */
-    _implied_opcode: $ => (choice(
-      /[nN][oO][pP]/,
-      /[cC][lL][cC]/,
-      /[cC][lL][dD]/,
-      /[cC][lL][iI]/,
-      /[cC][lL][vV]/,
-      /[dD][eE][xX]/,
-      /[dD][eE][yY]/,
-      /[iI][nN][xX]/,
-      /[iI][nN][yY]/,
-      /[sS][eE][cC]/,
-      /[sS][eE][dD]/,
-      /[sS][eE][iI]/,
-      /[sS][tT][pP]/,
-      /[tT][aA][xX]/,
-      /[tT][aA][yY]/,
-      /[tT][sS][xX]/,
-      /[tT][xX][aA]/,
-      /[tT][xX][sS]/,
-      /[tT][yY][aA]/,
-      /[wW][aA][iI]/,
-      // a register
-      /[aA][sS][lL]/,
-      /[dD][eE][cC]/,
-      /[iI][nN][cC]/,
-      /[lL][sS][rR]/,
-      /[rR][oO][lL]/,
-      /[rR][oO][rR]/,
-      // stack based
-      /[bB][rR][kK]/,
-      /[pP][hH][aA]/,
-      /[pP][hH][pP]/,
-      /[pP][hH][xX]/,
-      /[pP][hH][yY]/,
-      /[pP][lL][aA]/,
-      /[pP][lL][pP]/,
-      /[pP][lL][xX]/,
-      /[pP][lL][yY]/,
-      /[rR][tT][iI]/,
-      /[rR][tT][sS]/,
-    )),
-
-    _relative_opcode: $ => (choice(
-      /[bB][cC][cC]/,
-      /[bB][cC][sS]/,
-      /[bB][eE][qQ]/,
-      /[bB][mM][iI]/,
-      /[bB][nN][eE]/,
-      /[bB][pP][lL]/,
-      /[bB][rR][aA]/,
-      /[bB][vV][cC]/,
-      /[bB][vV][sS]/,
-    )),
-
-    _immediate_opcode: $ => (choice(
-      /[aA][dD][cC]/,
-      /[aA][nN][dD]/,
-      /[bB][iI][tT]/,
-      /[cC][mM][pP]/,
-      /[cC][pP][xX]/,
-      /[cC][pP][yY]/,
-      /[eE][oO][rR]/,
-      /[lL][dD][aA]/,
-      /[lL][dD][xX]/,
-      /[lL][dD][yY]/,
-      /[oO][rR][aA]/,
-      /[sS][bB][cC]/,
-    )),
-
-    _absolute_opcode: $ => (choice(
-      /[aA][dD][cC]/,
-      /[aA][nN][dD]/,
-      /[aA][sS][lL]/,
-      /[bB][iI][tT]/,
-      /[cC][mM][pP]/,
-      /[cC][pP][xX]/,
-      /[cC][pP][yY]/,
-      /[dD][eE][cC]/,
-      /[eE][oO][rR]/,
-      /[iI][nN][cC]/,
-      /[jJ][mM][pP]/,
-      /[jJ][sS][rR]/,
-      /[lL][dD][aA]/,
-      /[lL][dD][xX]/,
-      /[lL][dD][yY]/,
-      /[lL][sS][rR]/,
-      /[oO][rR][aA]/,
-      /[rR][oO][lL]/,
-      /[rR][oO][rR]/,
-      /[sS][bB][cC]/,
-      /[sS][tT][aA]/,
-      /[sS][tT][xX]/,
-      /[sS][tT][yY]/,
-      /[sS][tT][zZ]/,
-      /[tT][rR][bB]/,
-      /[tT][sS][bB]/,
-    )),
-
-    _indirect_opcode: $ => (choice(
-      // ind abs
-      /[jJ][mM][pP]/,
-      // ind zp
-      /[aA][dD][cC]/,
-      /[aA][nN][dD]/,
-      /[cC][mM][pP]/,
-      /[eE][oO][rR]/,
-      /[lL][dD][aA]/,
-      /[oO][rR][aA]/,
-      /[sS][bB][cC]/,
-      /[sS][tT][aA]/,
-    )),
-
-    local_label:  $ => (/\.[a-zA-Z0-9_]+/),
-    global_label: $ => (/[a-zA-Z_][a-zA-Z0-9_]+/),
-  },
+		local_label: ($) => /\.[a-zA-Z0-9_]+/,
+		global_label: ($) => /[a-zA-Z_][a-zA-Z0-9_]+/,
+	},
 });
